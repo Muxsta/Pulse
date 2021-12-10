2883911904ba10507ec607715dc8b6070472a662
Bret Palsson <bretep@gmail.com>
2021-12-10 01:31:11 -0800

    Balance performance and usability

--- apps/indexer/lib/indexer/fetcher/coin_balance.ex
+++ apps/indexer/lib/indexer/fetcher/coin_balance.ex
@@ -22,7 +22,7 @@ defmodule Indexer.Fetcher.CoinBalance do
   @defaults [
     flush_interval: :timer.seconds(3),
     max_batch_size: 1,
-    max_concurrency: 2000,
+    max_concurrency: 200,
     task_supervisor: Indexer.Fetcher.CoinBalance.TaskSupervisor,
     metadata: [fetcher: :coin_balance]
   ]
