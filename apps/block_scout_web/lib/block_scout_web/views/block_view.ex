38af0456a5065b51312d5aaea9b1ce1312e82765
Bret Palsson <bretep@gmail.com>
2022-02-01 20:32:37 -0800

    Use elixir-format to override units

--- apps/block_scout_web/lib/block_scout_web/views/block_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/block_view.ex
@@ -18,7 +18,7 @@ defmodule BlockScoutWeb.BlockView do
       |> Kernel.||(0)
       |> BlockScoutWeb.Cldr.Number.to_string!()
 
-    unit_text = gettext("Beat")
+    unit_text = gettext("Gwei")
 
     "#{average} #{unit_text}"
   end
